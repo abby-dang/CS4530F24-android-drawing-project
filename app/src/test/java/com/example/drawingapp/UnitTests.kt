65570f4006cef9bafea7eac2769bcb622033f8ab
Yakira Sun <zhenzhen201303@gmail.com>
2024-10-05 19:57:33 -0600

    Updated test file name

---
+++ app/src/test/java/com/example/drawingapp/UnitTests.kt
@@ -0,0 +1,21 @@
+package com.example.drawingapp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Unit Tests for drawing app.
+ * Makes sure functions are working as intended.
+ */
+class UnitTests {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun test() {
+
+    }
+}
